cf4b8e98567f1989273498a8d211ec1e64ad6277
Daniel <daniel@kerber-family.net>
2022-04-29 03:00:31 -0500

    AUDIO AND SPINNY TOTEM THING PLEASE LET ME SLEEP

---
+++ Assets/Scripts/door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    Movement movement;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            movement = collision.gameObject.GetComponent<Movement>();
+            if(movement.hasKey)
+            {
+                audioSource.clip = audioClip;
+                audioSource.Play();
+                movement.hasKey = !movement.hasKey;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/pedestal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pedestal : MonoBehaviour
+{
+    Movement movement;
+    public GameObject idolText;
+    Animator animator;
+    public GameObject finalDoorClosed;
+    public GameObject idolRoom;
+    public AudioClip clip;
+    public AudioSource source;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        idolText.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            movement = collision.GetComponent<Movement>();
+            if(movement.hasKey)
+            {
+                source.clip = clip;
+                source.Play();
+                animator.SetTrigger("hasTotem");
+                finalDoorClosed.SetActive(false);
+                idolText.SetActive(false);
+            }
+            else
+            {
+                finalDoorClosed.SetActive(true);
+                idolText.SetActive(true);
+                idolRoom.SetActive(false);
+            }
+        }
+    }
+}
